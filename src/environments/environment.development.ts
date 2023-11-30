345435106587f32f66147790fe87e35aa1504411
Andrés Bustamante <afbustag@gmail.com>
2023-11-30 16:13:00 +0100

    Updated environments configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  coreApiUrl: 'http://localhost:8280/api',
+  usersApiUrl: 'http://localhost:8080/realms/footero',
+  usersApiClientId: 'footero-dev',
+  production: false
+};
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  coreApiUrl: 'https://api.footero.net/core',
-  usersApiUrl: 'https://sso.footero.net/realms/footero',
-  usersApiClientId: 'footero',
-  production: true
-};
